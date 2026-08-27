55afa978b351c7559b59d5094602f35ed81da555
Yesha joshi <yesha.joshi137255@marwadiuniversity.ac.in>
2026-08-27 23:25:07 +0530

    Implement findPoisonedDuration method in Solution class

---
+++ teem attacking.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        int ans=0;
+        for(int i=0;i<timeSeries.size()-1;i++)
+            ans+=min(duration,timeSeries[i+1]-timeSeries[i]);
+        return ans+duration;
+    }
+};
