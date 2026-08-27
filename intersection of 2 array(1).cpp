57c18b79305ae1b25e62337e5ba1b0d4aa25f50a
Yesha joshi <yesha.joshi137255@marwadiuniversity.ac.in>
2026-08-27 23:18:58 +0530

    Implement intersection of two arrays

---
+++ intersection of 2 array(1).cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        set<int> s(nums1.begin(),nums1.end());
+        vector<int> ans;
+        for(int x:nums2) {
+            if(s.count(x)) {
+                ans.push_back(x);
+                s.erase(x);
+            }
+        }
+        return ans;
+    }
+};
